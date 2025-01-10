4dcd311ea7d84311647a9f6ff728c3674541cf7d
CarlosfcPinheiro <carlosfcpinheiro82@gmail.com>
2025-01-10 17:18:14 -0300

    Adicionar generics

---
+++ src/Collections/Main.java
@@ -0,0 +1,14 @@
+package Collections;
+
+
+public class Main {
+    public static void main(String[] args){
+        GenericList<String> list = new GenericList<>();
+        list.add("a");
+        list.add("b");
+
+        for (var item : list){
+            System.out.println(item);
+        }
+    }
+}
